6cdb4c29f91aa554892f45f5728b48fa569f037d
luiizsilverio <luiiz.silverio@gmail.com>
2022-05-12 19:05:52 -0300

    projeto concluído

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,15 +1,43 @@
 import React, { useState } from 'react';
 import { Alert } from 'react-native';
 import { Button } from '../Button';
-import { Input } from '../Input';
+// import { Input } from '../Input';
+import { ControlledInput as Input } from '../ControlledInput';
 import { Container } from './styles';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
-const VALID_EMAIL_EXPRESSION = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+type FormData = {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
+
+const schema = yup.object({
+  name: yup.string().required("Informe o seu nome"),
+  email: yup.string().email("E-mail inválido")
+    .required("Informe seu e-mail"),
+  password: yup.string().min(6, "Senha deve ter no mínimo 6 dígitos")
+    .required("Informe a senha"),
+  password_confirm: yup.string().required("Confirme a senha")
+    .oneOf([yup.ref('password')], "A senha não confere")
+})
 
 export function Form() {
+  const { control, handleSubmit, formState } = useForm<FormData>({
+    resolver: yupResolver(schema)
+  })
 
-  function handleUserRegister() {
+  const errors = formState.errors
 
+  if (errors) {
+    console.log(errors)
+  }
+  
+  function handleUserRegister(data: FormData) {
+    console.log(data)
   }
 
   return (
@@ -17,27 +45,39 @@ export function Form() {
       <Input
         icon="user"
         placeholder="Nome"
+        name="name"
+        control={control}
+        error={errors.name}
       />
       <Input
         icon="mail"
         placeholder="E-mail"
         keyboardType="email-address"
         autoCapitalize='none'
+        name="email"
+        control={control}
+        error={errors.email}
       />
       <Input
         icon="lock"
         placeholder="Senha"
         secureTextEntry
+        name="password"
+        control={control}
+        error={errors.password}
       />
       <Input
         icon="lock"
         placeholder="Confirme a senha"
         secureTextEntry
+        name="password_confirm"
+        control={control}
+        error={errors.password_confirm}
       />
 
       <Button
         title="Cadastrar"
-        onPress={handleUserRegister}
+        onPress={handleSubmit(handleUserRegister)}
       />
     </Container>
   )
